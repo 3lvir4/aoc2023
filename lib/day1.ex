e7803035ad8d98960323cdf6167e371edb9da0bf
3lvir4 <pierreavril18@gmail.com>
2023-12-02 04:16:48 +0100

    day 1 second star completed

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,31 +1,58 @@
 defmodule Day1 do
+  defp str_digit_map() do
+   %{
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9
+    }  
+  end
+
   def handle(data_path) do
     File.stream!(data_path)
+    |> Enum.filter(&String.trim(&1) != "")
     |> Enum.reduce(0, &proc_line/2)
   end
 
   defp proc_line(line, acc) do
-    line
-    |> String.to_charlist()
-    |> Enum.filter(fn c -> c >= ?0 and c <= ?9 end)
-    |> get_first_last_digit()
-    |> case do
-      {nil, nil} -> acc
-      pair -> pair
-          |> pair_to_2digit_num()
-          |> Kernel.+(acc)
-      end
+    res = 0..String.length(line)-1
+    |> Enum.map(fn i -> extract_digits(line, i) end)
+    |> Enum.filter(fn d -> d !== nil end)
+    case { List.first(res), List.last(res) } do
+      {nil, _} -> acc
+      {_, nil} -> acc
+      {f, l} -> acc + concat_two_dgts(f, l)
+    end
   end
 
-  defp pair_to_2digit_num({first, last}) do
-    first
-    |> Kernel.-(48)
-    |> Kernel.*(10)
-    |> Kernel.+(last - 48)
+  def extract_digits(str, i \\ 0) do
+    case String.at(str, i) do
+      nil -> nil
+      c ->
+        case Integer.parse(c) do
+          {c, ""} -> c
+          _ ->
+            match_str_to_dgt(String.slice(str, i..-1))
+        end
+    end
   end
 
-  defp get_first_last_digit(cl), do: {List.first(cl), List.last(cl)}
-
+  defp concat_two_dgts(a, b) do
+    tmp = Integer.to_string(a) <> Integer.to_string(b)
+    String.to_integer(tmp)
+  end
+  
+  def match_str_to_dgt(str) do
+    map = str_digit_map()
+    Map.keys(map)
+    |> Enum.find(fn sdgt -> String.starts_with?(str, Atom.to_string(sdgt)) end)
+    |> (fn sdgt -> Map.get(map, sdgt) end).()
+  end
 end
 
 
